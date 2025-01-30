3f0021a53b66de3a8c49c9d2cff9b1280135fa2f
Rohit Anchal <105239963+rohitanchal@users.noreply.github.com>
2025-01-30 11:44:39 -0500

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
-//your JS code here. If required.
+// Creating the student object
 const student = {
   name: "Alice"
 };
 
+// Function to get all the keys of an object
 function getKeys(obj) {
-  return Object.keys(obj);
+  return Object.keys(obj); // Using Object.keys to return the keys as an array
 }
 
+// Testing the function by passing the student object
 const keys = getKeys(student);
-console.log(keys);
+console.log(keys); // Output: ["name"]
