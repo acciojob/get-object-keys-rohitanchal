3189faef9250f91f5e8f73c474a72591cc72f727
Rohit Anchal <105239963+rohitanchal@users.noreply.github.com>
2025-01-30 11:38:48 -0500

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+const student = {
+  name: "Alice"
+};
+
+function getKeys(obj) {
+  return Object.keys(obj);
+}
+
+const keys = getKeys(student);
+console.log(keys);
